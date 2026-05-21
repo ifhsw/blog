b9a900bd390eccf53570776e934b4b4f2c014c7d
shizu <3523634839@qq.com>
2026-05-21 13:30:45 +0800

    feat: add extensions barrel export

---
+++ src/lib/editor/extensions/index.ts
@@ -0,0 +1,5 @@
+// src/lib/editor/extensions/index.ts
+export { Callout } from "./callout";
+export { CodeBlock } from "./code-block";
+export { ImageCaption } from "./image-caption";
+export { MathInline, MathBlock } from "./math";
