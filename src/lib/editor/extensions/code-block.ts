492a1e170c8d09bb676ced684f20fd8d56bc4249
shizu <3523634839@qq.com>
2026-05-21 13:26:15 +0800

    feat: add TipTap CodeBlock extension with Shiki via lowlight

---
+++ src/lib/editor/extensions/code-block.ts
@@ -0,0 +1,34 @@
+// src/lib/editor/extensions/code-block.ts
+import CodeBlockLowlight from "@tiptap/extension-code-block-lowlight";
+import { common, createLowlight } from "lowlight";
+
+const lowlight = createLowlight(common);
+
+export const CodeBlock = CodeBlockLowlight.extend({
+  addAttributes() {
+    return {
+      ...this.parent?.(),
+      language: {
+        default: "plaintext",
+        rendered: true,
+      },
+    };
+  },
+
+  renderHTML({ node, HTMLAttributes }) {
+    const lang = node.attrs.language || "plaintext";
+    const tree = lowlight.highlight(lang, node.textContent);
+    const html = lowlight.toHtml(tree);
+    return [
+      "div",
+      { class: "code-block-wrapper" },
+      ["div", { class: "code-block-header" },
+        ["span", { class: "code-block-dots" }, "●●●"],
+        ["span", { class: "code-block-lang" }, lang],
+      ],
+      ["pre", HTMLAttributes,
+        ["code", { class: `language-${lang}` }, html],
+      ],
+    ];
+  },
+});
